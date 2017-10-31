52c52e0bbfe80d753c41cb030c76b623737017a3
jojolafreux <prugbyman@hotmail.fr>
2017-10-31 16:50:23 +0100

    test au bon endroit pour import

---
+++ Modules/test-s7-read-pcap.py
@@ -0,0 +1,46 @@
+from netzob.all import *
+from identmsg import *
+# we extract from the pcap every message which is going to 8000, i.e. the client messages 
+msgs1=PCAPImporter.readFile('../Exemples/s7-example-connection-readmessages.pcap', bpfFilter='dst port 102').values()
+msgs2=PCAPImporter.readFile('../Exemples/s7-example-connection-writemessages.pcap', bpfFilter='dst port 102').values()
+
+print("readmessages:")
+for m in msgs1:
+    print(m)
+sym1=Symbol(messages=msgs1)
+
+print("-----------------------------------------------------------------------")
+
+print("writemessages:")
+for m in msgs2:
+    print(m)
+sym2=Symbol(messages=msgs2)
+print("message 1 est binaire?")
+print(Binmessage(msgs1))
+
+print("message 2 est binaire?")
+print(Binmessage(msgs2))
+"""
+print("All Format.splitStatic:")
+Format.splitStatic(sym)
+for field in sym.fields:
+    print(field)
+
+
+print("Format.splitStatic:")
+print(sym.fields[0]) # 1st field ok
+print(sym.fields[1]) # 2nd field not OK, variadic field
+print(sym.fields[2])
+
+print("All Format.splitAligned::")
+Format.splitAligned(sym)
+for field in sym.fields:
+    print(field)
+
+Format.splitAligned(sym)
+print("Format.splitAligned:")
+print(sym.fields[1]) # OK
+print(sym.fields[2]) # OK
+
+print(sym.fields[3]) # not OK, believe 's' is a delimiter
+"""
