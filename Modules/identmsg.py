28d9e79151b2a13c1aafa9aeb5ee93de4e62d5ec
jojolafreux <prugbyman@hotmail.fr>
2017-10-31 16:11:48 +0100

    static

--- Modules/identmsg.py
+++ Modules/identmsg.py
@@ -1,7 +1,7 @@
 from netzob.all import *
 
 # we extract from the pcap every message which is going to 8000, i.e. the client messages 
-msgs=PCAPImporter.readFile('Exemples/test.pcap', bpfFilter='dst port 8000').values()
+msgs=PCAPImporter.readFile('../Exemples/test.pcap', bpfFilter='dst port 8000').values()
 
 """print("All messages:")
 for i in range(0,len(msgs)-1):
@@ -17,11 +17,11 @@ def staticmessage(msgs):
 	return result
 "print(staticmessage(msgs))"
 def Binmessage(msgs):
-	result= True
+	result= False
 	for j in range(0, len(msgs)-1):
 		for i in range(0,len(msgs[j].data)-1):
-			if (msgs[j].data[i]<128):
-				result= False
+			if (msgs[j].data[i]>128):
+				result= True
 	return result
 print(Binmessage(msgs))
 	
