d619d63c6b8f4e9be09cee40d74e6abaa85e2ae0
bartuc <mronur89@gmail.com>
2017-11-01 15:59:27 +0100

    Fix

--- Modules/modify.py
+++ Modules/modify.py
@@ -3,8 +3,8 @@
 from netzob.all import *
 
 def modifyMessageByte(m, index, newByte):
-    assert type(index) is IntType, "Index is not an integer"
-    assert type(newByte) is IntType, "newByte is not an integer"
+    assert isinstance(index,int), "Index is not an integer"
+    assert isinstance(newByte,int), "newByte is not an integer"
     if (index +1) > len(m):
         raise ValueError('Index is bigger than byte array length!!')
         return -1
