98f249f5a29a40aa86a6d5b6ca21b8d52fe6d327
Bastien DROUOT <bastien.drouot@ensta-bretagne.org>
2017-11-01 10:54:10 +0100

    multisplitalign

---
+++ Modules/multiSplitAligned.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from netzob.all import *
+
+def multiSplitAligned(msgs):
+	""" multiSplitAligned
+	Perform many splitAligneds and remove eatch time the "strange" messages 
+	Args: 
+		msg (string)      : List of messages.
+
+	Returns:
+
+	Authors: Bastien DROUOT
+
+	Date: 31/10/2017 : 9h45
+	"""
+	sym=Symbol(messages=msgs)
+	Format.splitAligned(sym)
+	i=0
+	for field in sym.fields:
+		if i%2:
+			#Si i est impaire = si field est non static
+			if i>4:
+				rep=list() # Liste de ligne
+				for line in field.getValues():
+					tmp=list() # Liste de chaque octé d'une ligne
+					for elem in line:
+						tmp.append(elem)
+					rep.append(tmp)
+				#TODO
+				break
+		i=i+1
+	return True
+
+if __name__ == '__main__':
+	msgs=PCAPImporter.readFile('../S7-Pcap/C1.pcap', bpfFilter='dst port 102').values()
+	multiSplitAligned(msgs)
