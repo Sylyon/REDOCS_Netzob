cab02d6b8edbf5cd94ef2e91a6b8a07efe0c375d
Bastien DROUOT <bastien.drouot@ensta-bretagne.org>
2017-11-02 00:22:10 +0100

    save

--- Modules/multiSplitAligned.py
+++ Modules/multiSplitAligned.py
@@ -38,25 +38,36 @@ def multiSplitAligned(msgs):
 	for field in sym.fields:
 		if i%2:
 			#Si i est impaire = si field est non static
-			if i>4:
-				rep=list() # Liste de ligne
-				for line in field.getValues():
-					rep.append(line)
-				#TODO
-				break
+			rep=list() # Liste de ligne
+			for line in field.getValues():
+				rep.append(line)
+			D=frequent_partial_msg(rep)
+			keys=list(D.keys())
+			keys.reverse()
+			for aKey in keys:
+				for aKey2 in D[aKey].keys():
+					if D[aKey][aKey2] > (len(D)/2):
+						repIndex=[j for j in range(len(field.getValues())) if field.getValues()[j][:len(aKey2)] == aKey2]
+						rep2=list()
+						k=0
+						for line2 in Symbol(messages=msgs).fields[0].getValues():
+							print(k)
+							print(repIndex)
+							if k in repIndex:
+								rep2.append(line2)
+							k=k+1
+						return rep2
 		i=i+1
-	return rep
+	return False
 
 if __name__ == '__main__':
 	msgs=PCAPImporter.readFile('../S7-Pcap/C1.pcap', bpfFilter='dst port 102').values()
 	rep=multiSplitAligned(msgs)
-
-	L=rep
-	print(L)
-	D=frequent_partial_msg(L)
+	print(rep)
+	"""D=frequent_partial_msg(L)
 	print (D)
 	for length in D: # we browse for every length found
 		print(D[length])
 		# get most frequent
 		mf=max(D[length],key=lambda x:D[length][x])
-		print("most frequent:", mf)
+		print("most frequent:", mf)"""
