c06926a804c356e81211e9f5c59ccd978fd0d5e5
rdcsdx <rdcsdx@aaa.com>
2017-11-01 11:55:22 +0100

    simple clustering for cascaded SplitAligned

--- Modules/multiSplitAligned.py
+++ Modules/multiSplitAligned.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 from netzob.all import *
+from collections import Counter
+import copy
+
+def frequent_partial_msg(L):
+	"""
+	frequent_partial_msg - get the frequency of beginning of the messages
+	return a dictionnary with the histograms for each lenth
+	param: list of raw messages
+	"""
+	d,r={},{} # two dict to avoid 'RuntimeError: dictionary changed size during iteration'
+	for l in range(2,max([len(x) for x in L])):
+		d[l]=dict(Counter([x[:l] for x in L]))
+		# get rid of unique items
+		r[l]=copy.copy(d[l]) 
+		for k in d[l]:
+			if d[l][k]<2: r[l].pop(k)
+		if r[l]=={}: r.pop(l)
+	return r
 
 def multiSplitAligned(msgs):
 	""" multiSplitAligned
@@ -35,3 +53,8 @@ def multiSplitAligned(msgs):
 if __name__ == '__main__':
 	msgs=PCAPImporter.readFile('../S7-Pcap/C1.pcap', bpfFilter='dst port 102').values()
 	multiSplitAligned(msgs)
+
+	L=[m.data for m in msgs]
+	D=frequent_partial_msg(L)
+	for length in D: # we browse for every length found
+		print(D[length])
